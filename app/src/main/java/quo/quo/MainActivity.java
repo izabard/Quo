b665ec2a12a61b7569a061e6d2f239175f8d2986
izabard <izaba97@gmail.com>
2016-12-17 16:54:05 -0600

    Abrír otra actividad en ImageButtom

--- app/src/main/java/quo/quo/MainActivity.java
+++ app/src/main/java/quo/quo/MainActivity.java
@@ -1,7 +1,10 @@
 package quo.quo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+       addListenerOnButtom();
     }
+
+   public void addListenerOnButtom(){
+
+        ImageButton imageButtom = (ImageButton) findViewById(R.id.img_education);
+        {
+            imageButtom.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(
+                            getApplicationContext(),
+                            EducationActivity.class);
+                    startActivity(intent);
+
+                }
+            });
+
+
+        }
+    }
+
+
+
+
 }
